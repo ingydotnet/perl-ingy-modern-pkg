1ee850708bd15a1824c7601b40e7f60acf3172cc
Ingy dot Net <ingy@ingy.net>
2011-10-24 16:15:20 -0700

    ingy-modern

---
+++ lib/%module.path%.pm
@@ -0,0 +1,24 @@
+##
+# name:      [% module.name %]
+# abstract:  [% abstract %]
+# author:    [% author.name %] <[% author.email %]>
+# license:   [% license.type %]
+# copyright: [% date.year %]
+
+use [% perl.version %];
+use strict;
+use warnings;
+
+package [% module.name %];
+
+our $VERSION = '[% version %]';
+
+1;
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+...
